4c35ac073bcd619bd24bf8dba729e7227c106bec
Paola Díaz <paolamdiazp@gmail.com>
2025-10-05 17:13:23 -0300

    Menu del Club Deportivo

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,37 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion = 0;
+
+        while (opcion != 4) {
+            System.out.println("CLUB DEPORTIVO ISLA MALA");
+            System.out.println("1. Canchas");
+            System.out.println("2. Socios");
+            System.out.println("3. Tarifas");
+            System.out.println("4. Reservas");
+            System.out.println("0. Salir");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Intenta de nuevo");
+            }
+        }
+    }
 }
